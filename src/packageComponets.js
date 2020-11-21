1e3f2318915a33340bdc062ca52e69bee5311b23
Juan Carlos Rodriguez <jcrodriguezam@outlook.es>
2020-11-21 23:09:52 +0100

    Set package working

---
+++ src/packageComponets.js
@@ -0,0 +1,9 @@
+const Button = require('./stories/Button/Button');
+const Button2 = require('./stories/Button2/Button2');
+const Header = require('./stories/Header/Header');
+
+module.exports = {
+    Button,
+    Button2,
+    Header
+}
